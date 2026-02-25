531787845f6bb2c3ae0ce01d0305e4ad5b695174
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-02-24 18:10:07 -0700

    Fixing some logic in LetterKey, adding some additional stylings for other breakpoints.

--- src/components/LetterKey.jsx
+++ src/components/LetterKey.jsx
@@ -1,6 +1,6 @@
 const LetterKey = ({props, onChildClick}) => {
-    const handleClick = (props) => {
-        onChildClick('some-data');
+    const handleClick = () => {
+        onChildClick();
     }
     return (
         <button onClick={handleClick} data-character={props} className="word-block" >{props}</button>
